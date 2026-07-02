04e42c126436582b8e7239370888df6a3087b62b
just61888 <shastaoke4sha@hotmail.com>
2026-07-03 03:41:38 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
 # ======================== 配置区 ========================
-TOKEN = "8905803099:AAE7jU6eWwgtCHReLRcLkyWCSJHHPiKg4To"  # 罗湖机器人的 Token
+TOKEN = "8931334315:AAHFz42QPsgUHt7L5PxkOow_SPTS6Q1svAM"  # 龙华机器人的 Token
 ADMIN_ID = 7140260550  # 统一管理员 ID
 
 name_map = {
